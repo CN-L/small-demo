5c327c4f7f42f64de53f687e127628749968b816
李小亮 <1396595541@qq.com>
2019-03-06 00:23:54 +0800

    vue组件传值，父子传值，子父传值完成
    父组件向子组件传值，子组件通过props接收传值，并且可以通过给data函数中的数据，达到修改传值的目的，但是不能（最好不要直接修改props接收的值），子向父：this.$emit('事件名',参数传值),自定义事件，在父组件通过@事件名 =一个方法（方法名随意）接收传过来的值即可，若想修改，还是将传过来的参数赋值给data函数中的对象

---
+++ demo/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 父组件传给子组件的值 -->
+  <div id="app">
+    <!-- 注册事件 -->
+    <num @numchange="handlechange" :count="n"></num>
+    {{n}}
+  </div>
+</template>
+
+<script>
+// 局部注册 引入子组件
+import Num from '@/components/Num';
+export default {
+  data(){
+    return{
+      n:5
+    }
+  },
+  components:{
+    Num
+  },
+  methods:{
+    // 接收子组件的传值
+    handlechange(num1){
+      // 把子组件的值赋值给父组件中的对象
+      this.n = num1;
+    }
+  },
+  name:'App'
+}
+</script>
+
+<style>
+</style>

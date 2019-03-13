23c0fe5c02e895cc695b3ad805d96f8c94970e70
李小亮 <1396595541@qq.com>
2019-03-13 22:46:20 +0800

    update

--- 面试题集合/对象合并且去重.js
+++ 面试题集合/对象合并且去重.js
@@ -9,37 +9,40 @@
 		c:4
 	}
 	var object3 = {...object1,...object2};
+
 /*数组中对象去重*/
-var arr = [{
-      key: '01',
-      value: '乐乐'
-   }, {
-      key: '02',
-      value: '博博'
-   }, {
-      key: '03',
-      value: '淘淘'
-   },{
-      key: '04',
-      value: '哈哈'
-   },{
-      key: '01',
-      value: '乐乐'
-   }];
-   var arr1 = [];
-   for(var i = 0;i<arr.length;i++){
-	      var flag = true;
-	   	  for(var j = 0 ; j<arr1.length;j++){
-	   	  	if( arr [i].key == arr1[j].key){
-	   	  	   flag = false;
-	   	  	}
-	   	  }
-	   	  if(flag){
-   	  	    arr1.push(arr[i])
-   	       }
-   	 
-   }
-   console.log(arr1)
+	var arr = [{
+	      key: '01',
+	      value: '乐乐'
+	   }, {
+	      key: '02',
+	      value: '博博'
+	   }, {
+	      key: '03',
+	      value: '淘淘'
+	   },{
+	      key: '04',
+	      value: '哈哈'
+	   },{
+	      key: '01',
+	      value: '乐乐'
+	   }];
+	   var arr1 = [];
+	   for(var i = 0;i<arr.length;i++){
+		      var flag = true;
+		   	  for(var j = 0 ; j<arr1.length;j++){
+		   	  	if( arr [i].key == arr1[j].key){
+		   	  	   flag = false;
+		   	  	}
+		   	  }
+		   	  if(flag){
+	   	  	    arr1.push(arr[i])
+	   	       }
+	   	 
+	   }
+	console.log(arr1)
+
+
    // 单一对象内部去重
    var obj3 = {
    	name:"李鱼龙",
@@ -47,9 +50,9 @@ var arr = [{
    	school:"渤海学院",
    	name:"李鱼龙"
    }
-   var object4 = {};var flag = true;
+   var object4 = {};
    for(var item in obj3){
-   	 
+   	 var flag = true;
    	 for(var items in object4){
    	 	if(items == item){
    	 		flag == false;
